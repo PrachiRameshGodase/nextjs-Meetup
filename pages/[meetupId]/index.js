e95aa707449fff86244e0189ca2ed2ec3e56dce5
Prach Ramesh Godase <prachigodase2@gmail.com>
2023-07-05 15:23:14 +0530

    Prerendering the powers of Next JS

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -2,7 +2,6 @@
 import MeetupDetails from "../../components/meetups/MeetupDetails";
 
 function MeetupDetail(){
-
     return (
         <MeetupDetails 
             image='https://images.unsplash.com/photo-1618823617088-b170e5d7501a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1183&q=80'
@@ -10,6 +9,40 @@ function MeetupDetail(){
          address='Some Street 5, Some City' 
          description='The meetup description' />
     )
+}
+
+export async function getStaticPaths(){
+    return {
+        fallback:false,
+        paths:[
+            {params:{
+                meetupId:'m1'
+            }},
+            {params:{
+                meetupId:'m2'
+            }}
+        ]
+            
+        
+    }
+}
+
+export async function getStaticProps(context){
+    //fetch data for a meetup
+    const meetupId=context.params.meetupId
+    console.log(meetupId)
+    return{
+        props:{
+            meetupData:{
+                image:'https://images.unsplash.com/photo-1618823617088-b170e5d7501a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1183&q=80',
+                id:meetupId,
+                title:'a First Meetup',
+                address:'Some Street 5, Some City',
+                description:'The meetup description'
+
+            }
+        }
+    }
 }
 
 export default MeetupDetail;
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,56 @@
 
+// import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList"
-function HomePage(){
 
-    const DUMMy_MEETUPS=[{
-        id: "m1",
-      title: "Meetup 1",
-      image:
-        "https://images.unsplash.com/photo-1618823617088-b170e5d7501a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1183&q=80",
-      address: "London",
-      description: "This is the First meetup",
-    },
-    {
-        id: "m2",
-        title: "Meetup 2",
-        image:
-          "https://plus.unsplash.com/premium_photo-1671734045770-4b9e1a5e53a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
-        address: "Birmingham",
-        description: "This is theSecond meetup",
-      },,]
+const DUMMy_MEETUPS=[{
+    id: "m1",
+  title: "Meetup 1",
+  image:
+    "https://images.unsplash.com/photo-1618823617088-b170e5d7501a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1183&q=80",
+  address: "London",
+  description: "This is the First meetup",
+},
+{
+    id: "m2",
+    title: "Meetup 2",
+    image:
+      "https://plus.unsplash.com/premium_photo-1671734045770-4b9e1a5e53a0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
+    address: "Birmingham",
+    description: "This is theSecond meetup",
+  },]
+function HomePage(props){
+    // const [lodedMeetups,setlodedMeetups]=useState([])
+    // useEffect(()=>{
+    //     setlodedMeetups(DUMMy_MEETUPS)
+    // },[])
+
+    
     return(
-        
-            <MeetupList meetups={DUMMy_MEETUPS}/> 
-       
-      
+        <MeetupList meetups={props.meetups}/> 
     )
 }
+
+// export async function getServerSideProps(context){
+//     const req=context.req;
+//     const res=context.res;
+//     //data update after request
+//     return {
+
+//         props:{
+//             meetups:DUMMy_MEETUPS
+//         }
+
+//     }
+// } 
+
+
+export async function getStaticProps(){
+    //fetching data
+    return{
+    props:{
+      meetups: DUMMy_MEETUPS
+    },
+    revalidate: 1
+    };
+  }
 export default HomePage;
